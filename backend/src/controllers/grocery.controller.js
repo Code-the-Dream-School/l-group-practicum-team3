f14a1aec09b2477e85487b10f4f77d20151cba63
spycrab <wialex121@gmail.com>
2026-04-25 15:50:58 -0400

    add basic crud operations

---
+++ backend/src/controllers/grocery.controller.js
@@ -0,0 +1,19 @@
+const getGroceryItems = async (req, res) => {
+  return res.status(200).json({ message: "get all items" });
+};
+
+const getGroceryItemById = async (req, res) => {
+  return res.status(200).json({ message: "get item by id" });
+};
+
+const addGroceryItem = async (req, res) => {
+  return res.status(200).json({ message: "add grocery item" });
+};
+
+const updateGroceryItem = async (req, res) => {
+  return res.status(200).json({ message: "update grocery item" });
+};
+
+const deleteGroceryItem = async (req, res) => {
+  return res.status(200).json({ message: "returning all items" });
+};
