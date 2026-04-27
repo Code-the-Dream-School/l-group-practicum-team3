211ad324a77831cdc453cdc0a45c8d2b92ef2066
Yongting <yongtingshi657@gmail.com>
2026-04-27 14:22:32 -0400

    add custom theme for MUI

--- frontend/src/pages/public/Login.jsx
+++ frontend/src/pages/public/Login.jsx
@@ -1,8 +1,12 @@
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
 import React from 'react'
 
 function Login() {
   return (
+    <>
     <div>Login</div>
+    </>
   )
 }
 
