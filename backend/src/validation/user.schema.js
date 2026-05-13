dfb17ebd0a843ede48350ca21a62bcad243d7e4b
pleaseHelpImDumb <31854027+pleaseHelpImDumb@users.noreply.github.com>
2026-05-13 18:33:22 -0400

    Supabase google auth changes

--- backend/src/validation/user.schema.js
+++ backend/src/validation/user.schema.js
@@ -1,13 +1,21 @@
 const Joi = require("joi");
 
+const emailRequirements = Joi.string().email().required();
+const passwordRequirements = Joi.string()
+  .min(8)
+  .pattern(/[0-9]/) // at least 1 number
+  .pattern(/[^a-zA-Z0-9]/) // at least 1 symbol
+  .required();
+
 const registerSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string()
-    .min(8)
-    .pattern(/[0-9]/) // at least 1 number
-    .pattern(/[^a-zA-Z0-9]/) // at least 1 symbol
-    .required(),
+  email: emailRequirements,
+  password: passwordRequirements,
 });
 
-module.exports = { registerSchema };
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: passwordRequirements,
+});
+
+module.exports = { registerSchema, loginSchema };
