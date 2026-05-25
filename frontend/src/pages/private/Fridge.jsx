1c0eb0e34317266fad1d9f7f0da47ed5c1d0272a
spycrab <wialex121@gmail.com>
2026-05-25 17:40:14 -0400

    add frigde ingredient component, context, and page files, auto-formatting changes

--- frontend/src/pages/private/Fridge.jsx
+++ frontend/src/pages/private/Fridge.jsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { Box, Typography, Grid, Fab, Tooltip, Card, CardContent } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  Fab,
+  Tooltip,
+  Card,
+  CardContent,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
 import AppLogo from "../../components/AppLogo";
@@ -8,65 +16,68 @@ import CategoryFilter from "../../components/fridge/CategoryFilter";
 import ItemCard from "../../components/fridge/ItemCard";
 
 const data = {
-    "date": "06/01/2016",
-    "items": [
-        {
-            "name": "Zucchini",
-            "category": "produce",
-            "quantity": 0.778,
-            "unit": "kg",
-            "price": 4.66
-        },
-        {
-            "name": "Chicken",
-            "category": "proteins",
-            "quantity": 0.500,
-            "unit": "kg",
-            "price": 10.25
-        },
-        {
-            "name": "Cheese",
-            "category": "dairy",
-            "quantity": 0.5,
-            "unit": "kg",
-            "price": 4.66
-        },
-        {
-            "name": "milk",
-            "category": "dairy",
-            "quantity": 0.778,
-            "unit": "2l",
-            "price": 4.66
-        },
-        {
-            "name": "apple",
-            "category": "produce",
-            "quantity": 0.500,
-            "unit": "kg",
-            "price": 10.25
-        },
-        {
-            "name": "rice",
-            "category": "grains",
-            "quantity": 0.5,
-            "unit": "kg",
-            "price": 4.66
-        },
-        
-    ],
-    "subtotal": 24.2,
-    "total": 24.2
-}
+  date: "06/01/2016",
+  items: [
+    {
+      name: "Zucchini",
+      category: "produce",
+      quantity: 0.778,
+      unit: "kg",
+      price: 4.66,
+    },
+    {
+      name: "Chicken",
+      category: "proteins",
+      quantity: 0.5,
+      unit: "kg",
+      price: 10.25,
+    },
+    {
+      name: "Cheese",
+      category: "dairy",
+      quantity: 0.5,
+      unit: "kg",
+      price: 4.66,
+    },
+    {
+      name: "milk",
+      category: "dairy",
+      quantity: 0.778,
+      unit: "2l",
+      price: 4.66,
+    },
+    {
+      name: "apple",
+      category: "produce",
+      quantity: 0.5,
+      unit: "kg",
+      price: 10.25,
+    },
+    {
+      name: "rice",
+      category: "grains",
+      quantity: 0.5,
+      unit: "kg",
+      price: 4.66,
+    },
+  ],
+  subtotal: 24.2,
+  total: 24.2,
+};
 
 const items = data.items;
 
 function Header() {
   return (
     <Box>
-      <Typography sx={{color: "primary.dark"}} variant="h4" fontWeight={800}>Inventory</Typography>
-      <Typography color="text.secondary" mt={1}>Keep track of your fresh ingredients and pantry staples.</Typography>
+      <Typography sx={{ color: "primary.dark" }} variant="h4" fontWeight={800}>
+        Inventory
+      </Typography>
+      <Typography color="text.secondary" mt={1}>
+        Keep track of your fresh ingredients and pantry staples.
+      </Typography>
     </Box>
-  )
+  );
 }
 
 export default function Fridge() {
@@ -76,44 +87,51 @@ export default function Fridge() {
   const items = data?.items || [];
   const searchTerm = search.trim().toLowerCase();
 
-  const filteredItems = items.filter(item => {
+  const filteredItems = items.filter((item) => {
     if (searchTerm) {
       return item.name.toLowerCase().includes(searchTerm);
-    };
+    }
 
     return (
-      selectedCategory === "All Items" || item.category.toLowerCase() === selectedCategory.toLowerCase()
-    )
+      selectedCategory === "All Items" ||
+      item.category.toLowerCase() === selectedCategory.toLowerCase()
+    );
   });
 
   return (
-    <Box sx={{p: 2, pb: 10, width: "100%", bgcolor: "background.default", minHeight: "100vh",}}>
+    <Box
+      sx={{
+        p: 2,
+        pb: 10,
+        width: "100%",
+        bgcolor: "background.default",
+        minHeight: "100vh",
+      }}
+    >
       <AppLogo />
       <Header />
 
       <SearchBar search={search} setSearch={setSearch} />
 
-      <CategoryFilter 
+      <CategoryFilter
         selectedCategory={selectedCategory}
         setSelectedCategory={setSelectedCategory}
       />
 
       <Grid container spacing={2} sx={{ alignItems: "stretch" }}>
-        {
-          filteredItems.map((item, index) => (
-            <Grid 
-              size={{
-                xs: 6,
-                sm: 4,
-                md: 3,
-                lg: 2,
-              }} 
-              key={index}
-            >
-              <ItemCard item={item} />
-            </Grid>
-          ))
-        }
+        {filteredItems.map((item, index) => (
+          <Grid
+            size={{
+              xs: 6,
+              sm: 4,
+              md: 3,
+              lg: 2,
+            }}
+            key={index}
+          >
+            <ItemCard item={item} />
+          </Grid>
+        ))}
 
         {/* Add Item Card */}
 
@@ -140,46 +158,45 @@ export default function Fridge() {
               transition: "0.2s",
               "&:hover": {
                 transform: "scale(1.02)",
-                
-              }
+              },
             }}
           >
-            <Box sx={{textAlign: "center"}}>
-              <AddIcon sx={{fontSize: 40, color: "primary.dark",}} />
-              <Typography sx={{mt: 1, color: "primary.dark", fontSize: 20, fontWeight: "bold"}}>
+            <Box sx={{ textAlign: "center" }}>
+              <AddIcon sx={{ fontSize: 40, color: "primary.dark" }} />
+              <Typography
+                sx={{
+                  mt: 1,
+                  color: "primary.dark",
+                  fontSize: 20,
+                  fontWeight: "bold",
+                }}
+              >
                 Add Item
               </Typography>
             </Box>
-
           </Card>
         </Grid>
 
-        {
-          filteredItems.length === 0 && (
-            <Box mt={4} width="100%" sx={{textAlign: "center"}}>
-              <Typography color="text.secondary">
-                No items found
-              </Typography>
-            </Box>
-          )
-        }
+        {filteredItems.length === 0 && (
+          <Box mt={4} width="100%" sx={{ textAlign: "center" }}>
+            <Typography color="text.secondary">No items found</Typography>
+          </Box>
+        )}
       </Grid>
 
       <Tooltip title="Add Item" arrow>
-      <Fab
-        sx={{
-          position: "fixed",
-          bottom: 80,
-          right: 16,
-          bgcolor: "secondary.main",
-        }}
-        color="secondary"
-      >
-        <AddIcon />
-      </Fab>
+        <Fab
+          sx={{
+            position: "fixed",
+            bottom: 80,
+            right: 16,
+            bgcolor: "secondary.main",
+          }}
+          color="secondary"
+        >
+          <AddIcon />
+        </Fab>
       </Tooltip>
-
     </Box>
-  )
+  );
 }
-
